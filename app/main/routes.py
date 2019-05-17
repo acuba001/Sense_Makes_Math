924658ad07e688bc736f95ecc6185ff6ee68aa62
acuba001 <4492431+acuba001@users.noreply.github.com>
2019-05-17 11:32:50 -0700

    Passed the YouTube channel ID as a template parameter.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,12 @@
 from app.main import bp
 from app.main.youtube import getYouTubeData
-from flask import render_template
+from flask import current_app, render_template
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return render_template('index.html', videos=getYouTubeData())
+    return render_template(
+        'index.html', 
+        videos=getYouTubeData(), 
+        channelID=current_app.config['YOUTUBE_CHANNEL_ID']
+    )
