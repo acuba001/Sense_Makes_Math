bcc3e3831b9e4eb9c4dd65c84520f2add955f328
Henry Zorrilla <thmthmnky@gmail.com>
2019-05-21 00:30:40 -0700

    Added facebbok login button

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ import os
 class Config(object):
     
     # REQUIRED
-    GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
-
+    GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY') 
+    FACEBOOK_APP_ID=os.environ.get('FACEBOOK_APP_ID')
+    FACEBOOK_API_VERSION=os.environ.get('FACEBOOK_API_VERSION')
+    
     # OPTIONAL
     YOUTUBE_DATA_FETCH_PER_DAY=int(os.environ.get('YOUTUBE_DATA_FETCH_PER_DAY') or 48)
     YOUTUBE_CHANNEL_ID=os.environ.get('YOUTUBE_CHANNEL_ID') or 'UCI3K1J8do2RAa0wC60YGK5Q'
