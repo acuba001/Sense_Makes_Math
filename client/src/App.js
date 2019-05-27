62b965c1f985ed0d51b892410a5ff2e6cf4474b7
acuba001 <4492431+acuba001@users.noreply.github.com>
2019-05-27 13:32:02 -0700

    Initial commit for react-app

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import banner from './website-header.jpg'
+import './App.css';
+
+import Content from './components/layout/Content'
+import Sidebar from './components/layout/Sidebar'
+import Footer from './components/layout/Footer'
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="bg-white">
+        <img className="mx-auto mb-3 d-block img-fluid" src={banner} alt="" />
+      </div>
+      <div className="container mx-auto p-3">
+        <p className="text-center"><span className="font-weight-bold">Our Mission:</span> To expose the world to Beauty, Power, and Magic that is the sacred art of Mathematicians</p>
+        <div className="bg-white">
+          <div className="row">
+            <div className="col-md-2 text-center my-3"><a href="#!" className="nounderline text-dark nohover">HOME</a></div>
+            <div className="col-md-2 text-center my-3"><a href="#!" className="nounderline text-dark">SHOWS</a></div>
+            <div className="col-md-2 text-center my-3"><a href="#!" className="nounderline text-dark">SOCIAL MEDIA</a></div>
+            <div className="col-md-2 text-center my-3"><a href="#!" className="nounderline text-dark">STORE</a></div>
+            <div className="col-md-2 text-center my-3"><a href="#!" className="nounderline text-dark">SUPPORT</a></div>
+            <div className="col-md-2 text-center my-3"><a href="#!" className="nounderline text-dark">ABOUT</a></div>
+          </div>
+          <div className="row">
+            <div className="col-lg-7 m-3">
+              <Content/>
+            </div>
+            <div className="col-lg-4 m-3">
+              <Sidebar/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer bg-light pb-4">
+        <div className="container text-center">
+          <h4>FOOTER</h4>
+        </div>
+      </div>
+    )
+  }
+}
