b5b0386e57a96bf22e1304438f7ad358cde7e035
Henry Zorrilla <thmthmnky@gmail.com>
2019-05-27 21:41:40 -0700

    [WIP] Creating Youtbe/Facebook api endpoints: Youtube

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,6 @@
 from app.main import bp
 from app.main.youtube import getYouTubeData
+from app.main.facebook import getFacebookPosts
 from flask import current_app, render_template 
 
 @bp.route('/')
@@ -16,7 +17,16 @@ def index():
 @bp.route('/fb-posts')
 def fbPosts():
     return render_template(
-        'fb_9p;posts.html',
+        '_posts1415.html',
         posts = getFacebookPosts(),
         pageId = current_app.config['FACEBOOK_PAGE_ID']
         )
+    
+@bp.route('/yt-posts')
+def ytPosts():
+    return render_template(
+        '_video.html',
+        posts = getYouTubeData(),
+        channelID=current_app.config['YOUTUBE_CHANNEL_ID']
+        )
+
