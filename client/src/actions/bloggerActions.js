86fde745b01ceffb67065e71dcf01efe4f88792a
acuba001 <4492431+acuba001@users.noreply.github.com>
2019-07-22 19:36:19 -0700

    Added blogger scaffolding for redux. Added home page posts to get from redux.

---
+++ client/src/actions/bloggerActions.js
@@ -0,0 +1,7 @@
+import {GET_BLOGGER_POSTS} from './types'
+
+export const getBloggerPosts = () => async dispatch => {
+  // const res = await fetch('ENDPOINT_URL')
+  // const data = await res.json()
+  // dispatch({type: GET_YOUTUBE_VIDEOS, payload: data})
+}
--- client/src/components/content/Home.js
+++ client/src/components/content/Home.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ReactHtmlParser from 'react-html-parser'
 
-export default class Content extends Component {
+class Home extends Component {
   render() {
+    const {blogger: {recentBlogPosts}} = this.props
+    var i = 0
     return (
       <div className="container">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, deleniti earum vel natus quasi dolorum accusamus saepe, tempore expedita tenetur quis, ipsa eligendi fuga beatae aut est magni voluptate neque?
-        Ipsum cumque consequuntur eveniet? Dolores voluptates nobis labore ex nostrum! Laboriosam magni in esse autem impedit fugit dolores corporis nihil, deserunt, laudantium voluptatibus. Eum consectetur ad aliquid, sint enim ullam!
-        Nam libero eos neque, aliquam corrupti, quis soluta dolorem adipisci ipsum doloribus sit blanditiis vero consequatur doloremque optio id. Placeat quasi laborum veniam odio reprehenderit ratione magni eligendi doloribus a!
-        Incidunt voluptatum at ea neque quisquam maxime cum quae reiciendis. Doloremque corrupti illo nihil harum totam minima eum, rerum deleniti repudiandae nam laborum reprehenderit asperiores perferendis excepturi cupiditate neque odio.
-        Iusto voluptatum odio nisi enim architecto soluta, ipsa ex provident similique non, facere magnam repellat fuga rem quidem aperiam quaerat delectus? Sapiente quae sit itaque, saepe obcaecati eum voluptatem ullam.
-        Libero illum natus hic earum amet repudiandae est perspiciatis atque quae, perferendis non esse odit nesciunt quaerat quo asperiores ipsa consequuntur. Accusamus recusandae maxime reprehenderit ullam fuga vitae commodi eligendi.
-        Modi, saepe enim maxime id beatae dolorem minima quam soluta quia dolore temporibus repudiandae adipisci dolor earum tempore molestias, possimus, magni placeat repellendus sunt a? Nam molestiae excepturi harum adipisci.
-        Odio nihil nostrum fugit inventore optio, quaerat dolor ut cumque. Minima, molestiae sequi? Similique consequatur in molestias debitis cumque, sint, vero enim necessitatibus ipsam minus eius impedit illum ducimus facere.
-        Ad aspernatur nobis amet laborum voluptates explicabo? Pariatur impedit dolorem nemo nesciunt quisquam. Doloribus inventore, hic eveniet officia expedita amet, vel doloremque similique quidem sed provident minus suscipit, fuga laboriosam?
-        Dolor eligendi voluptate expedita saepe cupiditate praesentium eaque placeat, eum consectetur dolorum ipsa architecto fugit culpa quidem sapiente? Corporis sunt quia vitae aspernatur, nesciunt itaque nulla commodi eligendi rem tempora.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, deleniti earum vel natus quasi dolorum accusamus saepe, tempore expedita tenetur quis, ipsa eligendi fuga beatae aut est magni voluptate neque?
-        Ipsum cumque consequuntur eveniet? Dolores voluptates nobis labore ex nostrum! Laboriosam magni in esse autem impedit fugit dolores corporis nihil, deserunt, laudantium voluptatibus. Eum consectetur ad aliquid, sint enim ullam!
-        Nam libero eos neque, aliquam corrupti, quis soluta dolorem adipisci ipsum doloribus sit blanditiis vero consequatur doloremque optio id. Placeat quasi laborum veniam odio reprehenderit ratione magni eligendi doloribus a!
-        Incidunt voluptatum at ea neque quisquam maxime cum quae reiciendis. Doloremque corrupti illo nihil harum totam minima eum, rerum deleniti repudiandae nam laborum reprehenderit asperiores perferendis excepturi cupiditate neque odio.
-        Iusto voluptatum odio nisi enim architecto soluta, ipsa ex provident similique non, facere magnam repellat fuga rem quidem aperiam quaerat delectus? Sapiente quae sit itaque, saepe obcaecati eum voluptatem ullam.
-        Libero illum natus hic earum amet repudiandae est perspiciatis atque quae, perferendis non esse odit nesciunt quaerat quo asperiores ipsa consequuntur. Accusamus recusandae maxime reprehenderit ullam fuga vitae commodi eligendi.
-        Modi, saepe enim maxime id beatae dolorem minima quam soluta quia dolore temporibus repudiandae adipisci dolor earum tempore molestias, possimus, magni placeat repellendus sunt a? Nam molestiae excepturi harum adipisci.
-        Odio nihil nostrum fugit inventore optio, quaerat dolor ut cumque. Minima, molestiae sequi? Similique consequatur in molestias debitis cumque, sint, vero enim necessitatibus ipsam minus eius impedit illum ducimus facere.
-        Ad aspernatur nobis amet laborum voluptates explicabo? Pariatur impedit dolorem nemo nesciunt quisquam. Doloribus inventore, hic eveniet officia expedita amet, vel doloremque similique quidem sed provident minus suscipit, fuga laboriosam?
-                Dolor eligendi voluptate expedita saepe cupiditate praesentium eaque placeat, eum consectetur dolorum ipsa architecto fugit culpa quidem sapiente? Corporis sunt quia vitae aspernatur, nesciunt itaque nulla commodi eligendi rem tempora.</p>
+        {
+          recentBlogPosts.map(post => (
+            <div className="card rounded-0 mb-3" key={i++}>
+              <div className="card-body">
+                <h3 className="card-title">{post.title}</h3>
+                <div className="card-text">
+                  {ReactHtmlParser(post.content)}
+                </div>
+              </div>
+            </div>
+          ))
+        }
       </div>
     )
   }
 }
+
+export default connect((state) => ({ blogger: state.blogger }))(Home)
