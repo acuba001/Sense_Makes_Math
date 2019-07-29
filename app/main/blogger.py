0380df34ff14ae7d110e43de87b8ad0fda3d3c51
Henry Zorrilla <thmthmnky@gmail.com>
2019-07-28 21:38:33 -0700

    Issue#Five: Created / Implemented  the 'strip_html' function in the 'response_formator' library

--- app/main/blogger.py
+++ app/main/blogger.py
@@ -16,35 +16,43 @@
 # https://www.googleapis.com/blogger/v3/blogs/blogId/pages/pageId
 
 from app import app, cache
+from app.libraries import strip_html as strip
+
 import json
 import requests
 
 timeout = 60*app.config['BLOGGER_DATA_FETCH_PER_DAY']/24
 
-def stripHTML(someString):
-	m=string.find(someString, "<")
-	n=string.find(someString, ">")
-	o=string.rfind(someString, "<")
-	p=string.rfind(someString, ">")
-	if n != o:
-		return stripHTML(someString[n+1, o-1])
-
-
 @cache.cached(timeout=timeout, key_prefix='getBloggerData')
 def getBloggerData():
-    url = 'https://www.googleapis.com/blogger/v3/blogs/' + app.config['BLOGGER_PAGE_BLOG_ID'] + '/posts'
+    url = 'https://www.googleapis.com/blogger/v3/blogs/' + \
+        app.config['BLOGGER_PAGE_BLOG_ID'] + '/posts'
     params = {
         'key': app.config['GOOGLE_API_KEY']
     }
 
     res = requests.get(url, params=params)
-    res_items=res.json().get("items")
-    coolarray=[]
+    res_items = res.json().get("items")
+    coolarray = []
     for item in res_items:
-    	theTitle=item["title"]
-    	theContent=item["content"]
-        post={'title': theTitle, 'content': stripHTML(theContent)}
+        theTitle = item["title"]
+        theContent = item["content"]
+        post = {'title': theTitle, 'content': strip(theContent)}
         coolarray.append(post)
     return coolarray
+
+# [TODO] Create a subscription
+# 
+# POST {base_URL}/subscriptions?part=snippet
+#  Request body:
+#   {
+#     'snippet': {
+#       'resourceId': {
+#         'kind': 'youtube#channel',
+#         'channelId': 'UC_x5XG1OV2P6uZZ5FSM9Ttw' 
+#        }
+#      }
+#   }
+
 
 
