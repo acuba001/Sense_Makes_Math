5f8ec6b0aab151d258d04b1e15bb737e996f7676
Henry O. Zorrilla <thmthmnky@gmail.com>
2019-08-04 19:16:44 -0700

    [PATCH] Issue#Six: Removing 'bs4' from library dependencies

--- app/libraries/response_formator.py
+++ app/libraries/response_formator.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup as soup
+# from bs4 import BeautifulSoup as soup
 import re
 
 def strip_html(raw_html):
@@ -7,12 +7,12 @@ def strip_html(raw_html):
     # Strip HTML Tags
     template1 = re.compile(r"<.*?>")
     clean_html = re.sub(template1, '', raw_html)
-    
+
     template2 = re.compile(r"/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/ig") 
     clean_html = re.sub(template2, '', clean_html)
 
     clean_html = clean_html.replace('\n', ' ')
 
     clean_html = clean_html.replace('&nbsp;', '')
-    
-    return clean_html or soup(raw_html, 'html.parser').text
+
+    return clean_html # or soup(raw_html, 'html.parser').text
