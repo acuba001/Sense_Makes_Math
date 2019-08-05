011ca3124d361e963930ef85ef22cf69926f03a8
Henry Zorrilla <thmthmnky@gmail.com>
2019-08-04 18:43:10 -0700

    [PATCH] Issue#Four: Refactored the 'YouTube' video endpoint

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 from app.main import bp
-from app.main.youtube import getYouTubeData, getAllYouTubeVideosByPlaylistId
+from app.main.youtube import getAllYouTubeVideos
 from app.main.facebook import getFacebookPosts
 from app.main.blogger import getBloggerData
 from flask import current_app, render_template, jsonify, abort
@@ -9,7 +9,7 @@ from flask import current_app, render_template, jsonify, abort
 def index():
     return render_template(
         'index.html', 
-        videos=getYouTubeData(), 
+        videos=getAllYouTubeVideos(), 
         channelID=current_app.config['YOUTUBE_CHANNEL_ID'],
         fbAppId=current_app.config['FACEBOOK_APP_ID'],
         fbAppVer=current_app.config['FACEBOOK_API_VERSION']
@@ -22,7 +22,16 @@ def fbPosts():
  
 @bp.route('/yt-posts')
 def ytPosts():
-    posts = getYouTubeData()
+    # Real Sample of post 
+    # {
+    #   "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/3W8Ro0CnpzXIr0qcQ4b6nJvBqdQ\"", 
+    #   "id": "5quyA4w_tV8", 
+    #   "kind": "youtube#video", 
+    #   "player": {
+    #     "embedHtml": "<iframe width=\"480\" height=\"270\" src=\"//www.youtube.com/embed/5quyA4w_tV8\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+    #   }
+    # }
+    posts = getAllYouTubeVideos()
     return jsonify({'posts': posts})
     
 @bp.route('/blog-posts')
