cc09845499178163ce0d938545b05d3cd2402477
Henry Zorrilla <thmthmnky@gmail.com>
2019-08-07 21:17:56 -0700

    Optimizing the 'YouTube' controller

--- app/main/youtube.py
+++ app/main/youtube.py
@@ -29,7 +29,7 @@ def getAllYouTubePlaylists():
             })
     return playlistIds
 
-def getYouTubeVideoIdsByPlaylist(playlistId):
+def getYouTubeVideoUnitsByPlaylist(playlistId):
     playlistItems_request_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
     playlistItems_params = {
         'key': app.config['GOOGLE_API_KEY'],
@@ -37,17 +37,22 @@ def getYouTubeVideoIdsByPlaylist(playlistId):
         'playlistId': playlistId,
         'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
     }
-    videoIds = []
+    videoIdsBuckets = []
     try:
         # To see what the response object looks like, 
         # please visit : https://developers.google.com/youtube/v3/docs/playlistItems
-        playlistItems_res = requests.get(playlistItems_request_url, params=playlistItems_params).json() or {}
+        playlistItems_res = requests.get(playlistItems_request_url, params=playlistItems_params).json()
         playlistItems_list = playlistItems_res['items']
     except Exception as err:
         print(err)
     for item in playlistItems_list:
-        videoIds.append(item['snippet']['resourceId']['videoId'])
-    return videoIds
+        isVideo = item['snippet']['resourceId']['kind'] == 'youtube#video'
+        if isVideo:
+            videoIdsBuckets.append({
+                'videoId': item['snippet']['resourceId']['videoId'], 
+                'bucket': playlistId
+                })
+    return videoIdsBuckets
 
 def getVideoResourceById(videoId):
     video_request_url = 'https://www.googleapis.com/youtube/v3/videos'
@@ -66,13 +71,30 @@ def getVideoResourceById(videoId):
 @cache.cached(timeout=timeout, key_prefix='getAllYouTubeVideos')
 def getAllYouTubeVideos():
     allYouTubeVideos = []
+    yt_VideoUnits = []
+
+    # Grab all 'Youtube' videoUnits
     listOfPLaylistIds = getAllYouTubePlaylists()
-    for pl in listOfPLaylistIds:
-        listOfVideoIdsByPlaylistId = getYouTubeVideoIdsByPlaylist(pl['id'])
-        for vid in listOfVideoIdsByPlaylistId:
-            videoResource = getVideoResourceById(vid)[0]
-            if videoResource not in allYouTubeVideos:
-                videoResource['playlistId'] = pl['id']
-                allYouTubeVideos.append(videoResource)
-                
+    for playlist in listOfPLaylistIds:
+        listOfVideoUnits = getYouTubeVideoUnitsByPlaylist(playlist['id'])
+        yt_VideoUnits.extend(listOfVideoUnits)
+
+    # Grab all 'YouTube' videos by 'videoId'
+    for videoUnit in yt_VideoUnits:
+        videoResource = getVideoResourceById(videoUnit["videoId"])[0]
+        if videoResource not in allYouTubeVideos:
+            videoResource['playlistId'] = videoUnit["bucket"]
+            allYouTubeVideos.append(videoResource)
+            
+    # videoResource Sample
+    #
+    # {
+    #     'kind': 'youtube#video',
+    #     'etag': '[SOME_COOL_HASH]',
+    #     'id': '[SOME_OTHER_HASH]',
+    #     'player': {
+    #         'embedHtml': '[SOME_COOL_HTML]
+    #     },
+    #     'playlistId': '[AGAIN_WITH_YET_ANOTHER_COOL_HASH]'
+    # }
     return allYouTubeVideos
