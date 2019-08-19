2808266f9642b06655e53e8676c6247bdd317c78
acuba001 <4492431+acuba001@users.noreply.github.com>
2019-08-19 03:34:21 -0700

    Added spinners for the blog posts, shows, and sidebar.

--- client/src/components/content/Home.js
+++ client/src/components/content/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import ReactHtmlParser from 'react-html-parser'
 import {getBloggerPosts} from '../../actions/bloggerActions'
+import loadingSpinner from './images/Home/kloader.gif'
 
 class Home extends Component {
 
@@ -15,16 +16,18 @@ class Home extends Component {
     return (
       <div className="container">
         {
-          recentBlogPosts.map(post => post.content ? (
-            <div className="card rounded-0 mb-3" key={i++}>
-              <div className="card-body">
-                <h3 className="card-title">{post.title}</h3>
-                <div className="card-text">
-                  {ReactHtmlParser(post.content)}
+          recentBlogPosts.length === 0 ? 
+            <img className="d-block mt-3 mx-auto" style={{width: "10%"}} src={loadingSpinner} alt="Loading Blog Posts" /> : 
+            recentBlogPosts.map(post => (
+              <div className="card rounded-0 mb-3" key={i++}>
+                <div className="card-body">
+                  <h3 className="card-title">{post.title}</h3>
+                  <div className="card-text">
+                    {ReactHtmlParser(post.content)}
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : null)
+            ))
         }
       </div>
     )
--- client/src/components/content/Shows.js
+++ client/src/components/content/Shows.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {getYouTubeVideos} from '../../actions/youtubeActions'
+import loadingSpinner from './images/Shows/kloader.gif'
 
 class Shows extends Component {
   componentDidMount(){
@@ -11,17 +12,22 @@ class Shows extends Component {
     const { youtube: { recentVideos } } = this.props
     return (
       <div className="container p-3">
-        {recentVideos
-          .filter((video, ind) => ind < process.env.REACT_APP_SHOWS_NUM)
-          .map((video, ind) => (
-            <div key={ind} className="embed-responsive embed-responsive-16by9 mb-3">
-              <iframe className="embed-responsive-item" title={video.id} src={'https://www.youtube.com/embed/' + video.id} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
-          ))
+        {
+          recentVideos.length === 0 ? 
+            <img className="d-block mt-3 mx-auto" style={{width: "10%"}} src={loadingSpinner} alt="Loading Show Posts" />:
+            recentVideos
+              .filter((video, ind) => ind < process.env.REACT_APP_SHOWS_NUM)
+              .map((video, ind) => (
+                <div key={ind} className="embed-responsive embed-responsive-16by9 mb-3">
+                  <iframe className="embed-responsive-item" title={video.id} src={'https://www.youtube.com/embed/' + video.id} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+              ))
         }
-        <a href="https://www.youtube.com/channel/UCI3K1J8do2RAa0wC60YGK5Q" className="btn btn-light btn-block" target="_blank" rel="noopener noreferrer">
-          Click Here For More!
-        </a>
+        {recentVideos.length !== 0 ? 
+          (<a href="https://www.youtube.com/channel/UCI3K1J8do2RAa0wC60YGK5Q" className="btn btn-light btn-block" target="_blank" rel="noopener noreferrer">
+            Click Here For More!
+          </a>) : null
+        }
       </div>
     )
   }
