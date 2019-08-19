449ea01985d11e1c5599edbb971b74015e08e0e6
acuba001 <4492431+acuba001@users.noreply.github.com>
2019-08-19 15:19:05 -0700

    Fixed widget cutting out in smaller screens.

--- client/src/components/content/Podcast.js
+++ client/src/components/content/Podcast.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Podcast() {
   return (
-    <div className="embed-responsive embed-responsive-16by9">
+    <div className="embed-responsive embed-responsive-1by1">
         <iframe id="player_iframe" title="Podcast Widget" src="https://www.buzzsprout.com/301124?client_source=large_player&amp;iframe=true&amp;referrer=https://www.buzzsprout.com/301124.js?player=large" className="embed-responsive-item" scrolling="no"></iframe>
     </div>
   )
