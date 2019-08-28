a266080a1a8c7914e5efdbdf5110279b64644e60
Henry Zorrilla <thmthmnky@gmail.com>
2019-08-27 23:58:40 -0700

    Added a response/ error_message formatting library

--- app/libraries/__init__.py
+++ app/libraries/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('libraries', __name__)
 
-from app.libraries.response_formator import strip_html
+from app.libraries.response_formator import strip_html, formatResponse, formatErrorMessage
--- app/libraries/response_formator.py
+++ app/libraries/response_formator.py
@@ -2,12 +2,12 @@ import re
 
 def strip_html(raw_html):
     re.purge()
-    
+
     # Strip HTML Tags
     template1 = re.compile(r"<.*?>")
     clean_html = re.sub(template1, '', raw_html)
 
-    template2 = re.compile(r"/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/ig") 
+    template2 = re.compile(r"/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/ig")
     clean_html = re.sub(template2, '', clean_html)
 
     clean_html = clean_html.replace('\n', ' ')
@@ -15,3 +15,24 @@ def strip_html(raw_html):
     clean_html = clean_html.replace('&nbsp;', '')
 
     return clean_html
+
+def formatErrorMessage(error, context):
+    err_message = None
+    ln = str(context.lineno or None)
+    fn = str(context.filename.rpartition("/")[2].rpartition(".")[0] or None)
+    fnc = str(context.function or None)
+    template = "[LN {}] An error occured while executing {}.{}. Error thrown: {}" if context else "Sorry, something went wrong! Please trya gain later. Error Thrown: {}"
+    err_message = template.format(ln, fn, fnc, error) if context else template.format(error)
+
+    return err_message if error != None else str(error)
+
+
+def formatResponse(response, context):
+    code = response["code"]
+    data = [] if not code in [200] else response["result"]
+    error = formatErrorMessage(response['result'], context) if not code in [200] else str(None)
+    return {
+        'code': code,
+        'data': data,
+        'error': error
+    }
