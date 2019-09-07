321629f65c73f77ac6d3d41990db5fb2ed414c37
Henry Zorrilla <thmthmnky@gmail.com>
2019-09-07 16:11:50 -0700

    Error Handling: Adding Context to the Base Error

--- app/errors/error_types.py
+++ app/errors/error_types.py
@@ -1,43 +1,57 @@
 class Error(Exception):
-    """Base class for exceptions in this module."""
-    
-
-class BadUrlError(Error):
-    """Exception raised for errors in the input.
+    """
+    Base class for exceptions in this module.
 
     Attributes:
-        expression -- input expression in which the error occurred
-        message -- explanation of the error
+        context -- the meta date of the function which made the server call
     """
+    def __init__(self, context, *args, **kwargs):
+        self.context = context
 
-    def __init__(self, url, message):
+class BadUrlError(Error):
+    """
+    Exception raised for errors in the path used to make external api calls.
+
+    Attributes:
+            url -- the path which threw the error
+        message -- explanation of the error
+        context -- the meta date of the function which made the server call
+    """
+        
+    def __init__(self, url, message, context = None):
         self.url = url
         self.message = message
+        if not context is None:
+            super().__init__(context)
+        
 
 class ApiError(Error):
-    """Exception raised for errors returned when calling an A2pi.
+    """
+    Exception raised for errors returned when calling an external Api.
 
     Attributes:
             api -- the <api> which responded with the error
             url -- the <url> used to make a call to the <api>
-        message -- the error message from <api>
+        message -- the error <message> from <api>
+        context -- the meta date of the function which made the server call
     """
-
-    def __init__(self, api, url, message):
+    def __init__(self, api, url, message, context = None):
         self.api = api
         self.url = url
         self.message = message
+        if not context is None:
+            super().__init__(context)
 
 class ServerError(Error):
-    """Raised when an operation attempts a state transition that's not
-    allowed.
+    """
+    Raised when an operation attempts a state transition that's not allowed.
 
     Attributes:
-        previous -- state at beginning of transition
-        next -- attempted new state
-        message -- explanation of why the specific transition is not allowed
+        message -- explanation of the error
+        context -- the meta date of the function which made the server call
     """
-
-    def __init__(self, context, message):
+    def __init__(self, message, context = None):
         self.message = message
+        if not context is None:
+            super().__init__(context)
         
