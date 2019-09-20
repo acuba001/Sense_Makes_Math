f5595b10cac0790ac04d44afda47dddc2d57de3d
Henry Zorrilla <thmthmnky@gmail.com>
2019-09-19 23:18:23 -0700

    Response Formattor: Refactored the 'config' method and cleaned dead code

--- app/libraries/response_formator.py
+++ app/libraries/response_formator.py
@@ -10,7 +10,7 @@ def strip_html(raw_html):
 
     # Strip HTML extra characters
     template2 = re.compile(r"/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/ig") 
-    clean_html = re.sub(template2, '', clean_html)
+    clean_html = re.sub(template2, '', _html)
 
     clean_html = clean_html.replace('\n', ' ')
 
@@ -38,51 +38,35 @@ class myResponse():
         }
 
     def config(self, response):
-        isValid = type(response) is type({}) or response["error"] is None
-        isString = type(response["error"]) is type("")
-        if isValid:
-            self.content['code'] = response['code'] or 404
-            self.content['data'] = response['results'] or []
-            if response['error']['reason']:
-                self.content['error']['reason'] = response['error']['reason']
-            elif response['error']['message']:
-                self.content['error']['message'] = response['error']['message']
-            elif isString:
+        if type(response) is type({}):
+            self.content['error']['reason'] = 'Internal Server Error'
+            self.content['error']['message'] = 'Fatal Server Error: Something...went wrong'
+
+            self.content['code'] = response['code']
+            self.content['data'] = response['results']
+
+            if type(response['error']) is type({}):
+                if 'reason' in response['error'].keys():
+                    self.content['error']['reason'] = response['error']['reason']
+                
+                if 'message' in response['error'].keys():
+                    self.content['error']['message'] = response['error']['message']
+
+            if type(response['error']) is type(''):
                 self.content['error']['message'] = response['error']
-            else:
-                self.content['error'] = None
+            
+            if type(response['error']) is type(None):
+                self.content['error']['reason'] = None
+                self.content['error']['message'] = None
+
+            if 'paging' in response.keys() and type(response['paging']) is type({}):
+                if 'total' in response['paging'].keys():
+                    self.content['paging']['total'] = response['paging']['total']
+
+                if 'offset' in response['paging'].keys():
+                    self.content['paging']['offset'] = response['paging']['offset']
+
+                if 'limit' in response['paging'].keys():
+                    self.content['paging']['limit'] = response['paging']['limit']
+
         return self.content
-           
-
-# def formatErrorMessage(error, context):
-#     """Current Version"""
-#     err_message = None
-#     ln = str(context.lineno or None)
-#     fn = str(context.filename.rpartition("/")[2].rpartition(".")[0] or None)
-#     fnc = str(context.function or None)
-#     template = "[LN {}] An error occured while executing {}.{}. Error thrown: {}" if context else "Sorry, something went wrong! Please trya gain later. Error Thrown: {}"
-#     err_message = template.format(ln, fn, fnc, error) if context else template.format(error)
-
-#     return err_message if error != None else str(error)
-
-
-# def formatResponse(response, context):
-#     res_code = response["code"]
-#     if res_code != 200:
-#         res_data = []
-#         res_error = formatErrorMessage(response['result'], context)
-#         res_paging = str(None)
-#     else:
-#         res_data =response["result"]
-#         # print(res_data[0])
-#         res_error = str(None)
-#         res_paging = response["paging"] if response["paging"] else str(None)
-#         print(res_paging)
-#     res_object = {
-#         'code': res_code,
-#         'data': res_data,
-#         'error': res_error,
-#         'paging': res_paging
-#     }
-#     print(res_object)
-#     return res_object
