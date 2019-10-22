94752404350e44d00beddf51a94298d88a4c24cb
Henry Zorrilla <thmthmnky@gmail.com>
2019-10-22 01:00:25 -0700

    YouTube Api Controller: Added logic to 'YouTubeApiController' class

--- app/main/youtube.py
+++ app/main/youtube.py
@@ -3,7 +3,7 @@ import requests
 import inspect
 
 from app import app, cache
-from app.errors.error_types import BadUrlError, ExternalServerError, InternalServerError
+from app.errors import BadApiCallError, BadUrlError, ArithmeticOperationError, TypeMatchError, Error
 from app.libraries import myResponse
 
 timeout = 60*app.config['YOUTUBE_DATA_FETCH_PER_DAY']/24
@@ -13,127 +13,168 @@ class YouTubeController(Resource):
     """
     def __init__(self):
         self.base_url = "https://www.googleapis.com/youtube/v3/{}"
+    
+    def isValidEndpoint(self, endpoint):
+        valid_youtube_resources = [
+            'activity',
+            'channel',
+            'channelBanner',
+            'channelSection',
+            'guideCategory',
+            'i18nLanguage',
+            'i18nRegion',
+            'playlist',
+            'playlistItem',
+            'search result',
+            'subscription',
+            'thumbnail',
+            'video',
+            'videoCategory',
+            'watermark'
+        ]
+        return endpoint in valid_youtube_resources
 
-    def get(self, endpoint):
+    def isValidPart(self, part):
+        return True
+
+    def isValidChannelId(self, channelId):
+        return True
+            
+    def get(self, endpoint, opts = {}):
         context = inspect.stack()[0]
-        
-        url = self.base_url.format(str(endpoint))
+       
+        # [WIP] This current only works with base endpoints. 
+        # We need to expand the 'isValid' function to deal with
+        # other cases
+        # 
+        if self.isValidEndpoint(self=self, endpoint=endpoint):
+            url = self.base_url.format(str(endpoint))
+        else:
+            raise BadUrlError(None, context)
+
+        # url = self.base_url.format(str(endpoint))w
 
         params = {
             'key': app.config['GOOGLE_API_KEY'],
-            'part': 'id, player, snippet',
+            'part': 'id',
             'channelId': app.config['YOUTUBE_CHANNEL_ID'],
             'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
         }
+        for field in opts.keys():
+            if field is 'part':
+                part = opts['part']
+                if self.isValidPart(part):
+                    params['part'] = part
+            elif field is 'channelId':
+                channelId = opts['channelId']
+                if self.isValidChannelId(channelId):
+                    params['channelId'] = channelId
+        
 
         # To see what the response object looks like, 
         # please visit : https://developers.google.com/youtube/v3/docs/playlists#resource
         xRes = requests.get(url, params=params).json()
-        
-        playlistResources = []
-        list_of_playlists = xRes['items']
-        for item in list_of_playlists:
-            if item['kind'] == 'youtube#playlist': 
+        print(xRes)
+
+        # if xRes["code"] in [200]:
+        #     # Return formated response
+        #     return  xRes['items']
+        # else:
+        #     raise ExternalServerError("youtube", url, None, context)
+
+        # playlistResources = []
+        # list_of_playlists = xRes['items']
+        # for item in list_of_playlists:
+        #     if item['kind'] == 'youtube#playlist': 
+        #         playlistResources.append(item)
+        # return playlistResources
+
+def getAllYouTubePlaylistResources():
+    playlist_url = 'https://www.googleapis.com/youtube/v3/playlists'
+    playlist_params = {
+        'key': app.config['GOOGLE_API_KEY'],
+        'part': 'id, player, snippet',
+        'channelId': app.config['YOUTUBE_CHANNEL_ID'],
+        'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
+    }
+    playlistResources = []
+    try:
+        # To see what the response object looks like, 
+        # please visit : https://developers.google.com/youtube/v3/docs/playlists#resource
+        playlist_res = requests.get(playlist_url, params=playlist_params).json() or {}
+
+        playlist_list = playlist_res['items']
+        for item in playlist_list:
+            isPlaylist =  item['kind'] == 'youtube#playlist'
+            if isPlaylist: 
                 playlistResources.append(item)
 
         return playlistResources
+    except Exception:
+        raise  BadApiCallError(None, inspect.stack()[0])
 
-# from app import app, cache
-
-# import requests
-# import inspect
-
-# timeout = 60*app.config['YOUTUBE_DATA_FETCH_PER_DAY']/24
- 
-# def handleError(name, error):
-#     print("An error occured while executing @{}. Error thrown: {}".format(name, error))
-
-# def getAllYouTubePlaylistResources():
-#     playlist_url = 'https://www.googleapis.com/youtube/v3/playlists'
-#     playlist_params = {
-#         'key': app.config['GOOGLE_API_KEY'],
-#         'part': 'id, player, snippet',
-#         'channelId': app.config['YOUTUBE_CHANNEL_ID'],
-#         'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
-#     }
-#     playlistResources = []
-#     try:
-#         # To see what the response object looks like, 
-#         # please visit : https://developers.google.com/youtube/v3/docs/playlists#resource
-#         playlist_res = requests.get(playlist_url, params=playlist_params).json() or {}
-
-#         playlist_list = playlist_res['items']
-#         for item in playlist_list:
-#             isPlaylist =  item['kind'] == 'youtube#playlist'
-#             if isPlaylist: 
-#                 playlistResources.append(item)
-
-#         return playlistResources
-#     except Exception as err:
-#         handleError(inspect.stack()[0][3], err)
-
-# def getYouTubeVideoPlaylistItems(playlistId):
-#     playlistItems_request_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
-#     playlistItems_params = {
-#         'key': app.config['GOOGLE_API_KEY'],
-#         'part': 'snippet',
-#         'playlistId': playlistId,
-#         'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
-#     }
-#     playlistItemBucket = []
-#     try:
-#         # To see what the response object looks like, 
-#         # please visit : https://developers.google.com/youtube/v3/docs/playlistItems
-#         playlistItems_res = requests.get(playlistItems_request_url, params=playlistItems_params).json()
-#         playlistItems_list = playlistItems_res['items']
-#         for item in playlistItems_list:
-#             isVideo = item['snippet']['resourceId']['kind'] == 'youtube#video'
-#             if isVideo:
-#                 playlistItemBucket.append(item)
+def getYouTubeVideoPlaylistItems(playlistId):
+    playlistItems_request_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
+    playlistItems_params = {
+        'key': app.config['GOOGLE_API_KEY'],
+        'part': 'snippet',
+        'playlistId': playlistId,
+        'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
+    }
+    playlistItemBucket = []
+    try:
+        # To see what the response object looks like, 
+        # please visit : https://developers.google.com/youtube/v3/docs/playlistItems
+        playlistItems_res = requests.get(playlistItems_request_url, params=playlistItems_params).json()
+        playlistItems_list = playlistItems_res['items']
+        for item in playlistItems_list:
+            isVideo = item['snippet']['resourceId']['kind'] == 'youtube#video'
+            if isVideo:
+                playlistItemBucket.append(item)
         
-#         return playlistItemBucket
-#     except Exception as err:
-#         handleError(inspect.stack()[0][3], err)
+        return playlistItemBucket
+    except Exception:
+        raise BadApiCallError(None, inspect.stack()[0])
 
-# def getYouTubeVideoResource(videoId):
-#     video_request_url = 'https://www.googleapis.com/youtube/v3/videos'
-#     params = {
-#         'key': app.config['GOOGLE_API_KEY'],
-#         'part': 'id, player, snippet',
-#         'id': videoId,
-#         'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
-#     }
-#     try:
-#         # To see what the response object looks like, 
-#         # please visit : https://developers.google.com/youtube/v3/docs/videos#resource
-#         res = requests.get(video_request_url, params=params)
-#         return res.json()['items']
-#     except Exception as err:
-#         handleError(inspect.stack()[0][3], err)
+def getYouTubeVideoResource(videoId):
+    video_request_url = 'https://www.googleapis.com/youtube/v3/videos'
+    params = {
+        'key': app.config['GOOGLE_API_KEY'],
+        'part': 'id, player, snippet',
+        'id': videoId,
+        'maxResults': app.config['YOUTUBE_DATA_MAXRESULTS']
+    }
+    try:
+        # To see what the response object looks like, 
+        # please visit : https://developers.google.com/youtube/v3/docs/videos#resource
+        res = requests.get(video_request_url, params=params)
+        return res.json()['items']
+    except Exception:
+        raise BadApiCallError(None, inspect.stack()[0])
 
 
-# @cache.cached(timeout=timeout, key_prefix='getAllYouTubeVideos')
-# def getAllYouTubeVideos():
-#     allYouTubeVideoResources = []
-#     allYouTubePlaylistItems = []
-#     try:
-#         listOfPLaylistResources = getAllYouTubePlaylistResources()
-#         # Grab all 'Youtube' videoResources
-#         for playlistResource in listOfPLaylistResources:
-#             listOfPlaylistItems = getYouTubeVideoPlaylistItems(playlistResource['id'])
-#             for item in listOfPlaylistItems:
-#                 item['playlistResource'] = playlistResource
-#                 allYouTubePlaylistItems.append(item)
+@cache.cached(timeout=timeout, key_prefix='getAllYouTubeVideos')
+def getAllYouTubeVideos():
+    allYouTubeVideoResources = []
+    allYouTubePlaylistItems = []
+    try:
+        listOfPLaylistResources = getAllYouTubePlaylistResources()
+        # Grab all 'Youtube' videoResources
+        for playlistResource in listOfPLaylistResources:
+            listOfPlaylistItems = getYouTubeVideoPlaylistItems(playlistResource['id'])
+            for item in listOfPlaylistItems:
+                item['playlistResource'] = playlistResource
+                allYouTubePlaylistItems.append(item)
         
-#         # Sort the list, chronologically, by the 'publishedAt' date     
-#         allYouTubePlaylistItems_sorted = sorted(allYouTubePlaylistItems, key=lambda x: x["snippet"]["publishedAt"])
+        # Sort the list, chronologically, by the 'publishedAt' date     
+        allYouTubePlaylistItems_sorted = sorted(allYouTubePlaylistItems, key=lambda x: x["snippet"]["publishedAt"])
 
-#         # Grab all 'YouTube' videos by 'videoId'
-#         for playlistItem in allYouTubePlaylistItems_sorted:
-#             resource = getYouTubeVideoResource(playlistItem["snippet"]["resourceId"]["videoId"])[0]
-#             resource["playlistResource"] = playlistItem['playlistResource']
-#             if resource not in allYouTubeVideoResources:
-#                 allYouTubeVideoResources.append(resource)
-#         return allYouTubeVideoResources
-#     except Exception as err:
-#         handleError(inspect.stack()[0][3], err)
+        # Grab all 'YouTube' videos by 'videoId'
+        for playlistItem in allYouTubePlaylistItems_sorted:
+            resource = getYouTubeVideoResource(playlistItem["snippet"]["resourceId"]["videoId"])[0]
+            resource["playlistResource"] = playlistItem['playlistResource']
+            if resource not in allYouTubeVideoResources:
+                allYouTubeVideoResources.append(resource)
+        return allYouTubeVideoResources
+    except Exception:
+        raise BadApiCallError(None, inspect.stack()[0])
