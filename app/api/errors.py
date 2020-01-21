04d839a03e52cd440f31323c7cf847defaef687d
Henry Zorrilla <thmthmnky@gmail.com>
2020-01-21 00:53:57 -0800

    Enhanced error handling

---
+++ app/api/errors.py
@@ -0,0 +1,56 @@
+from app.libraries.error import Error
+
+
+class BadUrlError(Error):
+    """
+    Exception raised for errors in the path used to make external api calls.
+
+    Attributes:
+            url -- the path which threw the error
+        message -- explanation of the error
+        context -- the meta date of the function which made the server call
+    """
+    def __init__(self, message, context=None, url=None):
+        self.url = url
+        if message is None:
+            message = "[{} {}] " + self.__class__ + ": {}(" + url + ") -- LN {}"
+        super(Error, self).__init__(message, context)
+
+
+class BadApiCallError(Error):
+    """
+    Exception raised for errors returned when calling an external Api.
+
+    Attributes:
+            api -- the <api> which responded with the error
+            url -- the <url> used to make a call to the <api>
+        message -- the error <message> from <api>
+        context -- the meta date of the function which made the server call
+    """
+    def __init__(self, message, context=None, api=None, url=None):
+        self.type = str(self.__class__).split(".").pop()
+        self.api = str(api)
+        self.url = str(url)
+        if message is None:
+            error_type = self.type.split(">")[0]
+            message = "[{} {}] " + error_type + ": {}.{}(" + self.url + ")"
+        super().__init__(message, context)
+
+
+class ArithmeticOperationError(Error):
+    """
+    Raised when an operation attempts a state transition that's not allowed.
+
+    Attributes:
+        message -- explanation of the error
+        context -- the meta data of the function which made the server call
+    """
+    def __init__(self, message, context=None, params=None):
+        if message is None:
+            if params and set(("input_1", "input_2")) <= set(dir(params)):
+                self.params = params
+                message = "[{} {}] " + self.__class__ + ": " + params["input_1"] + ".{}(" + params["input_2"] + ") -- LN {}"
+            elif params is None:
+                message = "[{} {}] " + self.__class__ + ": <Unknown>.{}(<Unknown>) -- LN {}"
+
+        super(Error, self).__init__(message, context)
--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,12 +1,49 @@
-from flask import render_template
 from app.errors import errors_bp as bp
+from flask import render_template, request, jsonify
+from app import db
+
+
+def wants_json_response():
+    return request.accept_mimetypes['application/json'] >= \
+        request.accept_mimetypes['text/html']
 
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
+    # if wants_json_response():
+    #     return api_error_response(404)
     return render_template('errors/404.html'), 404
 
 
 @bp.app_errorhandler(500)
 def internal_error(error):
+    db.session.rollback()
+    # if wants_json_response():
+    #     return api_error_response(500)
     return render_template('errors/500.html'), 500
+
+
+@bp.app_errorhandler(400)
+def bad_request(message):
+    response = jsonify({'error': 'bad request', 'message': message})
+    response.status_code = 400
+    return response
+
+
+@bp.app_errorhandler(401)
+def unauthorized(message):
+    response = jsonify({'error': 'unauthorized', 'message': message})
+    response.status_code = 401
+    return response
+
+
+@bp.app_errorhandler(403)
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
+    response.status_code = 403
+    return response
+
+
+# @api.errorhandler(ValidationError)
+# def validation_error(e):
+#     return bad_request(e.args[0])
