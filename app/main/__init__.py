ff89e9730d8894fe7238e7ddef30e095bdd5877e
Henry Zorrilla <thmthmnky@gmail.com>
2020-01-22 00:20:32 -0800

    Adding Permission Injection to app context

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,10 @@ from flask import Blueprint
 
 main_bp = Blueprint('main', __name__)
 
-from app.main import routes
+from . import routes
+from app.api.models import Permission
+
+
+@main_bp.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
